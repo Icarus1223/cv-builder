a5e5751abcd78113613ee1585d2269c8a40df189
developer <developer.dev24@gmail.com>
2023-07-20 15:10:43 +0530

    initial frontend project setup

---
+++ frontend/src/Routes/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes , Route} from "react-router-dom"
+import Home from '../Pages/Home/Home';
+
+const Index = () => {
+  return (
+    <>
+    <Routes>
+    <Route path="/"  element={<Home/>} />
+    </Routes>
+    
+    
+    </>
+  )
+}
+
+export default Index
