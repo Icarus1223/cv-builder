98ed304119fd8f57761f2119bfc97820b3f92905
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-07-24 17:59:32 +0530

    split screen in mobile view

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './home.css'
 
 import SideBarTab from '../../Components/SideBarTab/SideBarTab'
@@ -27,16 +27,50 @@ const Home = () => {
     const sideOptionHandle = () => {
         setSideBarOptions(true)
     }
+
+
+    const [splitPosition, setSplitPosition] = useState(30);
+    const containerRef = useRef(null);
+    const draggingRef = useRef(false);
+
+    const handleTouchStart = (e) => {
+        draggingRef.current = true;
+    };
+
+    const handleTouchEnd = () => {
+        draggingRef.current = false;
+    };
+
+    const handleTouchMove = (e) => {
+        if (draggingRef.current) {
+            const containerHeight = containerRef.current.clientHeight;
+            const touchY = e.touches[0].clientY;
+            const newSplitPosition = (touchY / containerHeight) * 100;
+
+            if (newSplitPosition >= 30 && newSplitPosition <= 85) {
+                setSplitPosition(newSplitPosition);
+            } else if (newSplitPosition > 85) {
+                setSideBarOptions(false);
+                setSplitPosition(30);
+            }
+        }
+    };
+
+
+
     return (
         <>
-            <div className={!sideBarToggle ? `home-wrapper` : "home_wrapper_full"}>
+            <div className={!sideBarToggle ? "home-wrapper" : "home_wrapper_full"}
+                ref={containerRef}
+                onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}>
                 <div className=" bg-dark-black">
-                    <SideBarTab toggleProp={setSideBarToggle} sideBarOption={sideBarOptions} />
+                    <SideBarTab toggleProp={setSideBarToggle} sideBarOption={sideBarOptions} onTouchStart={handleTouchStart} splitPosition={splitPosition} />
                 </div>
-                <div className={sideBarToggle ? `bg-home-white-smoke relative w-full rounded-r-[15px]` : "bg-home-white-smoke relative w-full rounded-[15px]"}  >
+                <div className={` md:h-[${splitPosition}%]` + sideBarToggle ? `bg-home-white-smoke relative w-full rounded-r-[15px]` : "bg-home-white-smoke relative w-full rounded-[15px]"}  >
                     <div className='pt-[50px] home_inner_page'>
-                        <div className='shadow shadow-[0_1px_10px_0px_rgba(155, 126, 219, 0.20)] blank-page h-[520px] w-[400px] bg-white m-auto'></div>
-                        <div className='fixed right-[30px] bottom-[45px] ai_writer '>
+                        <div className='shadow shadow-[0_1px_10px_0px_rgba(155, 126, 219, 0.20)] blank-page h-[520px] w-[50%] bg-white m-auto'></div>
+                        <div className='fixed right-[30px] bottom-[74px] ai_writer '>
                             <Tooltip content="Ai Writer" placement="left" className='tootltip_content text-[10px]  font-medium text-white'>
                                 <button className='ai_star border w-[53px]   py-1.5 bg-transparent shadow-none' onClick={handleAiButton}> <AiStarIcon /></button>
                             </Tooltip>
@@ -44,7 +78,7 @@ const Home = () => {
                         {
                             aiToggle && (
                                 <>
-                                    <div className='ask_ai fixed right-[90px] bottom-[18px] z-10'>
+                                    <div className='ask_ai fixed right-[90px] bottom-[74px] z-10'>
                                         <div className='relative '>
                                             <div className='absolute top-[9px] left-3 star_icon'>
                                                 <Star />
@@ -62,8 +96,8 @@ const Home = () => {
 
 
                         }
-                        <div className='add_new_page_button text-center py-[18px] relative w-[400px] m-auto '>
-                            <button className='w-[400px] m-auto flex gap-5 justify-center  border border-[#E7E8E9] bg-white py-2 text-grey-color text-[11px] rounded-[10px]'>Add new page <span>+</span></button>
+                        <div className='add_new_page_button text-center py-[18px] relative w-[50%]  m-auto '>
+                            <button className=' w-[100%] m-auto  flex gap-5 justify-center  border border-[#E7E8E9] bg-white py-2 text-grey-color text-[11px] rounded-[10px]'>Add new page <span>+</span></button>
 
 
 
@@ -85,8 +119,24 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={aiToggle ? "hidden" : 'mobile_view_bottom_buttons absolute bottom-0 w-[100%] block md:hidden'}>
-                        <nav className='flex justify-between p-[15px]'>
+                    <div className='page_width bg-white rounded-br-[15px] absolute bottom-0 w-[100%]'>
+                        <div className='flex justify-end items-center gap-[7px] py-[12px] px-[40px] '>
+                            <Typography className='text-[8px]'>Page 1/1</Typography>
+                            <input
+                                type="range"
+                                className="progress"
+                                min="0"
+                                max="100"
+                                step="1"
+                            />
+                            <div className='width_per'>
+                                <Typography className='text-[8px]'>50%</Typography>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={aiToggle ? "hidden" : 'mobile_view_bottom_buttons absolute bottom-0 w-[100%] '}>
+                        {!sideBarOptions && <nav className='flex justify-between p-[15px]'>
                             <div onClick={sideOptionHandle} className='shadow drop-shadow-[0_4px_10px_#9B7EDB4D] tabs-btn bg-pruple-color p-[18px] rounded-xl border-2 border-pruple-color w-[55px] h-[55px] flex justify-center items-center'>
                                 <PlusIcon />
                             </div>
@@ -97,7 +147,7 @@ const Home = () => {
                             <div className='shadow drop-shadow-[0_4px_10px_#9B7EDB4D] flex justify-center items-center tabs-btn border-2 border-pruple-color p-[18px] rounded-xl w-[55px] h-[55px]'>
                                 <WindowIcon />
                             </div>
-                        </nav>
+                        </nav>}
                     </div>
 
                 </div>
