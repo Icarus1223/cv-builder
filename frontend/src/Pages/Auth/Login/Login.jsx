39bc39f976fa4fc99ec3d2666f9feee5ccbf40d4
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-07-25 15:29:46 +0530

    add color

--- frontend/src/Pages/Auth/Login/Login.jsx
+++ frontend/src/Pages/Auth/Login/Login.jsx
@@ -8,7 +8,7 @@ import Linkdin from '../../../assets/Svg/LoginPageSvg/Linkdin';
 
 const Login = () => {
 
-  
+
   return (
     <section class="bg-dark-black min-h-screen grid">
       <div class="flex flex-col items-center justify-center px-[15px] pb-8 md:pt-[70px] pt-[30px] md:mx-auto  bg-dark-black">
@@ -17,18 +17,18 @@ const Login = () => {
             <h1 class="text-[25px] font-medium text-white text-center pb-[30px]">
               Ready, Set..
             </h1>
-            <Link to="" className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
+            <a href={`${process.env.BASE_URL}/linkedin`} className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
               <Linkdin />
               <Typography className="text-[20px] font-medium text-LoginPage-color">Sign up with LinkedIn</Typography>
-            </Link>
-            <Link to="" className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
+            </a>
+            <a href={`${process.env.BASE_URL}/google`} className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
               <GoggleIcon />
               <Typography className="text-[20px] font-medium text-LoginPage-color">Sign up with Google</Typography>
-            </Link>
-            <Link to="" className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
+            </a>
+            <a href={`${process.env.BASE_URL}/twitter`} className='signup_with_social_acc mt-[40px] shadow drop-shadow-3xl flex bg-white py-[16px] rounded-[15px] items-center justify-center gap-[16px]'>
               <TwitterIcon />
               <Typography className="text-[20px] font-medium text-LoginPage-color">Sign up with Twitter</Typography>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
